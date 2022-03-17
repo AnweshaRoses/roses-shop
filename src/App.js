eb48295b0da671c1fbb0bfae64f3485995bc9815
Anwesha Mishra <anwesharoses@gmail.com>
2022-03-17 09:51:04 +0530

    front-end done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import Cart from "./pages/Cart";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Product from "./pages/Product";
 import Productlist from "./pages/ProductList";
+import Register from "./pages/Register";
 
 
 function App() {
@@ -9,7 +12,10 @@ function App() {
     <div className="App">
       {/* <Home/> */}
       {/* <Productlist/> */}
-      <Product/>
+      {/* <Product/> */}
+      {/* <Login/> */}
+      {/* <Register/> */}
+      <Cart/>
     </div>
     </>
   );
